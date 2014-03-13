11d02bfc2176ee04b9e45d320276c3b4d84d6f68
MaryHal <maryhal@users.noreply.github.com>
2014-03-12 23:14:06 -0700

    Windows installer batch file more complete. I think.

--- WindowsInstall.bat
+++ WindowsInstall.bat
@@ -2,5 +2,6 @@ mkdir "%HOMEDRIVE%%HOMEPATH%\.vim"
 xcopy /E /Y * "%HOMEDRIVE%%HOMEPATH%\.vim"
 move /Y "%HOMEDRIVE%%HOMEPATH%\.vim\vimrc" "%HOMEDRIVE%%HOMEPATH%\.vimrc"
 
-
+set PATH=C:\msysgit\bin;%PATH%
+%HOMEDRIVE%%HOMEPATH%\.vim\bundle\neobundle.vim\bin\neoinstall.bat
 
