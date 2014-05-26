16f4d355e9fc54cbdcbb0a6d473bd20b9c28c420
MaryHal <maryhal@users.noreply.github.com>
2014-05-25 20:14:16 -0700

    remove current bundles in "WindowsInstall"

--- WindowsInstall.bat
+++ WindowsInstall.bat
@@ -1,3 +1,4 @@
+rmdir /S "%USERPROFILE%\AppData\Roaming\.emacs.d\"
 mkdir "%HOMEDRIVE%%HOMEPATH%\.vim"
 xcopy /E /Y * "%HOMEDRIVE%%HOMEPATH%\.vim"
 move /Y "%HOMEDRIVE%%HOMEPATH%\.vim\vimrc" "%HOMEDRIVE%%HOMEPATH%\.vimrc"
