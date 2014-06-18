d831f8c38cd9894dbbc16528e5abae92308f8e97
MaryHal <maryhal@users.noreply.github.com>
2014-06-18 01:05:45 -0700

    More asyncronous updates

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+cnt=0
 
-pushd $(dirname "$0")/bundle
-find . -maxdepth 1 -type d -exec sh -c '(cd {} && pwd && git pull)' ';'
+pushd $(dirname "$0")/bundle &&
+# cd ~/.vim/bundle &&
+for d in *; do
+  if [ -d $d/.git ]; then
+    echo "Updating $d"
+    cd $d && git pull &
+    # Prevent having too many subprocesses
+    (( (cnt += 1) % 16 == 0 )) && wait
+  fi
+done
+wait
+
 popd
